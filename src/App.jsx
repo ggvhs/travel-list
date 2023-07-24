10e8bd638eec632a388f1c31b3e9cebef23528cd
gg_ <89791105+ggvhs@users.noreply.github.com>
2023-07-24 13:31:25 -0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
   return (
     <div className='app'>
       <Logo />
-      <Form />
+      <Form onAddItems={handleAddItems}/>
       <PackingList initialItems={initialItems} />
       <Stats />
     </div>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState } from 'react';
 
-function Form() {
+function Form({onAddItems}) {
 
     const [description, setDescription] = useState('')
     const [quantity, setQuantity] = useState(1)
@@ -20,6 +20,7 @@ function Form() {
         }
 
         console.log(newItem)
+        onAddItems(newItem)
     }
 
     const handleChange = (e) => {
